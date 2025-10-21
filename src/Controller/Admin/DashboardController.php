1850f703a1052bfdcfb5fa37c1f08470d871cf04
andreyshibaev <andreishibaev@yandex.ru>
2025-10-21 23:14:54 +0700

    add AdminDashboard attribute

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,18 +4,18 @@ namespace App\Controller\Admin;
 
 use App\Entity\ConfigProject;
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+#[AdminDashboard(routePath: '/admin', routeName: 'admin_account')]
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin_account')]
     public function index(): Response
     {
         //        return parent::index();
